bda968914e91ae48a843a4cc2e279d4386bb1c15
CalebChalmers <caleb.chalmers1@gmail.com>
2017-06-07 14:39:32 -0400

    Add Dialog Service classes

---
+++ KAGTools/Services/IViewService.cs
@@ -0,0 +1,17 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KAGTools.Services
+{
+    public interface IViewService : IService
+    {
+        void RegisterView(Type windowType, Type viewModelType);
+
+        void OpenWindow(ViewModelBase viewModel);
+        bool? OpenDialog(ViewModelBase viewModel);
+    }
+}
