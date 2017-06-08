1cf3fc25d7edada98b78b1dd68a2f081b94886df
CalebChalmers <caleb.chalmers1@gmail.com>
2017-06-07 20:28:52 -0400

    Fix deleted mod not removed from mods.cfg

--- KAGTools/ViewModels/ModsViewModel.cs
+++ KAGTools/ViewModels/ModsViewModel.cs
@@ -102,6 +102,7 @@ namespace KAGTools.ViewModels
             {
                 Directory.Delete(Selected.Directory);
                 Mods.Remove(Selected);
+                UpdateActiveMods();
             }
         }
 
