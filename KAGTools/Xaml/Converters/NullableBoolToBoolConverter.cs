fb4cf57105603602e9161b82269402b3de238ce6
CalebChalmers <caleb.chalmers1@gmail.com>
2017-07-03 09:38:43 -0400

    Add XAML resource dictionary organization

--- KAGTools/Xaml/Converters/NullableBoolToBoolConverter.cs
+++ KAGTools/Xaml/Converters/NullableBoolToBoolConverter.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
 
-namespace KAGTools
+namespace KAGTools.Xaml.Converters
 {
     public class NullableBoolToBoolConverter : IValueConverter
     {
