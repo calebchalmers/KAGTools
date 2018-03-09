7ea7913cc06330c8ed1719c2b82546a6d3f1bf2e
CalebChalmers <caleb.chalmers1@gmail.com>
2018-03-08 19:24:39 -0500

    Add ApiHelper class

---
+++ KAGTools/Helpers/ApiHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KAGTools.Helpers
+{
+    public static class ApiHelper
+    {
+        private static readonly HttpClient httpClient;
+
+        static ApiHelper()
+        {
+            httpClient = new HttpClient();
+        }
+        }
+    }
+}
