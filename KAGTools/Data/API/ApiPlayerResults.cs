97726744d9e9ea8e85568dd489bff2c209d5c8f5
CalebChalmers <caleb.chalmers1@gmail.com>
2018-03-11 10:58:45 -0400

    Move nested classes in ApiPlayerResults

--- KAGTools/Data/API/ApiPlayerResults.cs
+++ KAGTools/Data/API/ApiPlayerResults.cs
@@ -18,74 +18,74 @@ namespace KAGTools.Data.API
 
         [JsonProperty("playerStatus")]
         public PlayerStatus Status { get; set; }
+    }
 
-        public class PlayerBanFlags
-        {
-            [JsonProperty("all")]
-            public string[] All { get; set; }
+    public class PlayerBanFlags
+    {
+        [JsonProperty("all")]
+        public string[] All { get; set; }
 
-            [JsonProperty("current")]
-            public string[] Current { get; set; }
-        }
+        [JsonProperty("current")]
+        public string[] Current { get; set; }
+    }
 
-        public class PlayerInfo
-        {
-            [JsonProperty("active")]
-            public bool IsActive { get; set; }
+    public class PlayerInfo
+    {
+        [JsonProperty("active")]
+        public bool IsActive { get; set; }
 
-            [JsonProperty("banExpiration")]
-            public string BanExpiration { get; set; }
+        [JsonProperty("banExpiration")]
+        public string BanExpiration { get; set; }
 
-            [JsonProperty("banReason")]
-            public string BanReason { get; set; }
+        [JsonProperty("banReason")]
+        public string BanReason { get; set; }
 
-            [JsonProperty("banned")]
-            public bool IsBanned { get; set; }
+        [JsonProperty("banned")]
+        public bool IsBanned { get; set; }
 
-            [JsonProperty("gold")]
-            public bool HasGold { get; set; }
+        [JsonProperty("gold")]
+        public bool HasGold { get; set; }
 
-            [JsonProperty("gold_storm")]
-            public bool HasGoldStorm { get; set; }
+        [JsonProperty("gold_storm")]
+        public bool HasGoldStorm { get; set; }
 
-            [JsonProperty("gold_trenchrun")]
-            public bool HasGoldTrenchRun { get; set; }
+        [JsonProperty("gold_trenchrun")]
+        public bool HasGoldTrenchRun { get; set; }
 
-            [JsonProperty("regUnixTime")]
-            public int RegisterUnixTime { get; set; }
+        [JsonProperty("regUnixTime")]
+        public int RegisterUnixTime { get; set; }
 
-            [JsonProperty("registered")]
-            public string RegisterDate { get; set; }
+        [JsonProperty("registered")]
+        public string RegisterDate { get; set; }
 
-            [JsonProperty("role")]
-            public int Role { get; set; }
+        [JsonProperty("role")]
+        public int Role { get; set; }
 
-            [JsonProperty("username")]
-            public string Username { get; set; }
-        }
+        [JsonProperty("username")]
+        public string Username { get; set; }
+    }
 
-        public class PlayerStatus
-        {
-            [JsonProperty("action")]
-            public int Action { get; set; }
+    public class PlayerStatus
+    {
+        [JsonProperty("action")]
+        public int Action { get; set; }
 
-            [JsonProperty("lastUpdate")]
-            public string LastUpdateDate { get; set; }
+        [JsonProperty("lastUpdate")]
+        public string LastUpdateDate { get; set; }
 
-            [JsonProperty("server")]
-            public PlayerServer Server { get; set; }
+        [JsonProperty("server")]
+        public PlayerServer Server { get; set; }
+    }
 
-            public class PlayerServer
-            {
-                [JsonProperty("serverIPv4Address")]
-                public string IPv4Address { get; set; }
+    public class PlayerServer
+    {
+        [JsonProperty("serverIPv4Address")]
+        public string IPv4Address { get; set; }
 
-                [JsonProperty("serverIPv6Address")]
-                public string IPv6Address { get; set; }
+        [JsonProperty("serverIPv6Address")]
+        public string IPv6Address { get; set; }
 
-                [JsonProperty("serverPort")]
-                public string Port { get; set; }
-            }
-        }
+        [JsonProperty("serverPort")]
+        public string Port { get; set; }
     }
 }
