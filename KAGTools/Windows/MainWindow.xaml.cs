6f1cc0265fcb257b662acd7273dc52d02e781633
CalebChalmers <caleb.chalmers1@gmail.com>
2018-03-31 18:15:14 -0400

    Don't check for updates in debug mode

--- KAGTools/Windows/MainWindow.xaml.cs
+++ KAGTools/Windows/MainWindow.xaml.cs
@@ -40,7 +40,9 @@ namespace KAGTools.Windows
 
         private async void Window_Loaded(object sender, RoutedEventArgs e)
         {
+#if !DEBUG
             await UpdateHelper.UpdateApp();
+#endif
         }
     }
 }
